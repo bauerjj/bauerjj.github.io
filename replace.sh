9a2d8b330f8a6a9ad8714bb24ac6aa0b636c7908
justin bauer <kapotchy@gmail.com>
2017-07-12 13:50:12 -0700

    changed the two slashes to 4 so that it is easier to filter without confusing the http:

--- replace.sh
+++ replace.sh
@@ -4,7 +4,10 @@ FILES=$(find ./ -type f -name '*.html')
 for file in $FILES; do
  #echo $file
  if [ ${file: -5} == ".html" ]; then
-	sed -i -e 's/\/\//\//g' $file
-        #echo "yes"
+	sed -i -e 's/\/\/\/\/\//\//g' $file
+	#sed -i -e 's/https:\/\/.*\/\//https:\//g' $file
+        #sed -i -e 's/https:\//https:\/\//g' $file
+        #sed -i -e 's/http:\//http:\/\//g' $file
+	#echo "yes"
  fi
 done
