7d29916f9fae9d0627c471d4cb98874310752040
justin bauer <kapotchy@gmail.com>
2017-07-12 12:38:55 -0700

    fixed old script and base url in config

--- replace.sh
+++ replace.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 # Small script that replaces \\ with a single \ 
-for file in *; do
+FILES=$(find ./ -type f -name '*.html')
+for file in $FILES; do
+ #echo $file
  if [ ${file: -5} == ".html" ]; then
-	sed -i -e 's/\/\//\//g'
+	sed -i -e 's/\/\//\//g' $file
+        #echo "yes"
  fi
 done
