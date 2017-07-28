1758adba41be979e694b7be3cb06f032ad0b236e
justin bauer <kapotchy@gmail.com>
2017-07-28 00:42:05 -0700

    Scripted auto-commit on change (2017-07-28 00:41:56) by gitwatch.sh

--- replace.sh
+++ replace.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Small script that replaces \\ with a single \ 
-FILES=$(find ./ -type f -name '*')
+FILES=$(find ./ -type f -name '*.html')
 for file in $FILES; do
- 	#echo $file
+ #echo $file
+ if [ ${file: -5} == ".html" ]; then
 	sed -i -e 's/\/\/\/\/\//\//g' $file
 	#sed -i -e 's/https:\/\/.*\/\//https:\//g' $file
-	#sed -i -e 's/https:\//https:\/\//g' $file
-	#sed -i -e 's/http:\//http:\/\//g' $file
+        #sed -i -e 's/https:\//https:\/\//g' $file
+        #sed -i -e 's/http:\//http:\/\//g' $file
 	#echo "yes"
-
+ fi
 done
